9d7a82c5b817dbc9d140d6de3086d9b7b08894ec
tanakaderoy <tanaka.deroy@gmail.com>
2020-04-13 21:21:38 -0400

    remove & rotate key also encrypt key

--- app/src/main/java/com/tanaka/mazivanhanga/youtubeplaylist/utils/Constants.java
+++ app/src/main/java/com/tanaka/mazivanhanga/youtubeplaylist/utils/Constants.java
@@ -4,12 +4,14 @@ import android.util.Base64;
 
 import java.nio.charset.StandardCharsets;
 
+import static com.tanaka.mazivanhanga.youtubeplaylist.utils.APIKEY.API_KEY;
+
 public class Constants {
-    public static final String LETMEINNNNNNN = de64It("QUl6YVN5QmNOU1FDdFRjR1E1WFRVMGtSc0haYjhGWnNXYXFBUzFB");
+    public static final String LETMEINNNNNNN = API_KEY;
     public static final String BASEURL = "https://www.googleapis.com/youtube/v3/";
     public static final String PLAYLIST_ID = "playlistId";
     public static final String PLAYLIST_NAME = "playlistName";
-    public static final String LOADING = "Loading...";
+    static final String LOADING = "Loading...";
     public static final String CHANNEL_ID = "channelId";
     public static final String CHANNEL_NAME = "channelName";
     public static final String CHANNEL_IMAGE = "channelImage";
